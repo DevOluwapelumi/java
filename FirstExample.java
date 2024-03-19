dec70fe611feef512cfc804c8571b52d3f48a0a9
MultiMarkTechPro Media <victoroluwadarasimi16@gmail.com>
2024-03-19 15:24:13 +0100

    note commit

--- FirstExample.java
+++ FirstExample.java
@@ -1,7 +1,17 @@
+// public class FirstExample{
+
+//     public static void main(String[] args) {
+
+//         System.out.println("You are welcome to java class. This class promise to be exciting");
+//     }
+// }
+
+// import java.util.Scanner;
+
 public class FirstExample{
-
-    public static void main(String[] args) {
-
-        System.out.println("You are welcome to java class. This class promise to be exciting");
-    }
+  public static void main(String[] args){
+    int score = 50;
+    long newScore = score;
+    System.out.println(newScore);
+  }
 }
