f7c665f1d696763a44c7887b3d43dccaf4df0369
MultiMarkTechPro Media <victoroluwadarasimi16@gmail.com>
2024-03-19 16:11:57 +0100

    example commit

--- FirstExample.java
+++ FirstExample.java
@@ -6,12 +6,60 @@
 //     }
 // }
 
-// import java.util.Scanner;
+import java.util.Scanner;
 
-public class FirstExample{
-  public static void main(String[] args){
-    int score = 50;
-    long newScore = score;
-    System.out.println(newScore);
-  }
-}
+// public class FirstExample{
+//   public static void main(String[] args){
+//     int score = 50;
+//     long newScore = score;
+//     int anotherScore = (int)newScore;
+//     System.out.println(newScore);
+//   }
+// }
+
+//  public class FirstExample{
+//    public static void main(String[] args){
+//     String name = "Oluwapelumi";
+//     int age = 60;
+//     String status = "false";
+//     String balance = "500.45";
+//     String comment = "This is a Java class. it start from 8am to 10am Monday to Tuesday. There is no wasting of time.";
+//     System.out.println("Total length of comment is "+ comment.length());
+//     System.out.println(comment.toUpperCase());
+//     System.out.println(comment.toLowerCase());
+//     System.out.println(comment.indexOf("Monday"));
+//     System.out.println(comment.codePointAt(56));
+//     System.out.println(comment.codePointBefore(56));
+//     System.out.println(comment.codePointCount(45, 67));
+//     System.out.println(name.compareTo(status));
+//     System.out.println(name.compareToIgnoreCase(status));
+//     System.out.println(name.concat(status));
+//     System.out.println(name.contentEquals(status));
+//     System.out.println(name.startsWith("Y"));
+//     System.out.println(name.endsWith("e"));
+//     System.out.println(name.equals(status));
+//     System.out.println(name.equalsIgnoreCase(status));
+//     System.out.println(name.getBytes());
+//     System.out.println(name.intern());
+//     System.out.println(comment.replace("java", "javascript"));
+//     System.out.println(comment.split(" "));
+
+//         Scanner userInput = new Scanner (System.in);
+//         System.out.println("Enter your first name");
+//         System.out.println("Your first name is "+fname);
+//    }}
+
+    public class FirstExample{
+   public static void main(String[] args){
+        System.out.println(Math.abs(-50.0));
+        System.out.println(Math.min(70, 30));
+        System.out.println(Math.max(70.5, 30));
+        System.out.println(Math.sqrt(25));
+        System.out.println(Math.pow(50.0, 3.0));
+        System.out.println(Math.E);
+        System.out.println(Math.PI);
+        System.out.println(Math.TAU);
+        System.out.println(Math.addExact(40, 34));
+        System.out.println(Math.round(56.435334343));
+        System.out.println(Math.floorDiv(45, 21));
+   }}
