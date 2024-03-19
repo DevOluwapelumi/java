2094977c6f8d00aa469fbb932b35451edcb2d134
MultiMarkTechPro Media <victoroluwadarasimi16@gmail.com>
2024-03-19 15:05:59 +0100

    initial commit

---
+++ FirstExample.java
@@ -0,0 +1,7 @@
+public class FirstExample{
+
+    public static void main(String[] args) {
+
+        System.out.println("You are welcome to java class. This class promise to be exciting");
+    }
+}
