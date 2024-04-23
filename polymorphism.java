6ca0d81659bf72158ee16f9c6350bfd289ac9371
MultiMarkTechPro Media <victoroluwadarasimi16@gmail.com>
2024-04-23 16:05:28 +0100

    polymorphism commit

---
+++ polymorphism.java
@@ -0,0 +1,30 @@
+    //  //polymorphism
+
+ class chicken{
+    public void animalSound(){
+        System.out.println("The animal makes a sound");
+    }
+}
+
+class pig extends Animal{
+    public void animalSound(){
+        System.out.println("The pig says: wee wee");
+}
+}
+
+class Goat extends Animal{
+    public void animalSound(){
+        System.out.println("The goat says: moooo");
+    }
+}
+
+public class Animal{
+    public static void main(String [] args){
+        chicken an = new chicken();
+        pig pg = new pig();
+        Goat gt = new Goat();
+        an.animalSound();
+        pg.animalSound();
+        gt.animalSound();
+}
+}
