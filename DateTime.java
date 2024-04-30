4e652b841697eae8c8d46d80e267cfea76b0ee0c
MultiMarkTechPro Media <victoroluwadarasimi16@gmail.com>
2024-04-30 15:02:42 +0100

    DateTime commit

--- DateTime.java
+++ DateTime.java
@@ -7,6 +7,14 @@
         //     DateTimeFormatter
 
 
+         // Formatter can be in different format.
+    // yyyy-MM-dd = 2024-04-30,
+    // dd-MM-yyyy = 30-04-2024,
+    // dd-MMM-yyyy = 30-Apr-2024 14:51:03,
+    // E, MMM dd yyyy = Tue, Apr 30 2024 14:51:48
+
+
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.LocalDateTime;
@@ -20,6 +28,8 @@ public class DateTime {
         LocalTime lTime = LocalTime.now(); // To get current time on a device.
         LocalDateTime lDateTime = LocalDateTime.now(); // To get current time on a device and format it to normal.
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String myDateTime = lDateTime.format(formatter);
+
 
         System.out.println(lTime);
         System.out.println(lDate);
