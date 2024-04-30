f32aed5903ce315682258d56103428d3ce5b3736
MultiMarkTechPro Media <victoroluwadarasimi16@gmail.com>
2024-04-30 14:59:15 +0100

    DateTime commit

--- DateTime.java
+++ DateTime.java
@@ -9,17 +9,22 @@
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 
 public class DateTime {
 
     public static void main(String[] args) {
         LocalDate lDate = LocalDate.now(); // To get current date on a device.
         LocalTime lTime = LocalTime.now(); // To get current time on a device.
+        LocalDateTime lDateTime = LocalDateTime.now(); // To get current time on a device and format it to normal.
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
 
         System.out.println(lTime);
         System.out.println(lDate);
+        System.out.println(lDateTime);
+        System.out.println(formatter);
     }
 }
-
-
 
