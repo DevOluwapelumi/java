523f29d1165334c7441ca0eb9531b81ddfba4156
MultiMarkTechPro Media <victoroluwadarasimi16@gmail.com>
2024-05-06 15:23:24 +0100

    ArrayList commit3

--- ArrayList.java
+++ ArrayList.java
@@ -1,15 +1,16 @@
     // JAVA ArrayList //
-import java.util.ArrayLists;
+import java.util.LinkedList;
+import java.util.Collections;
 
 public class ArrayList{
     public static void main(String[] args){
-        ArrayLists<String> name_of_students = new ArrayLists<String>();
+        LinkedList<String> name_of_students = new LinkedList<String>();
         name_of_students.add("Oluwapelumi");
         name_of_students.add("Micheal");
         name_of_students.add("Esther");
         name_of_students.add("Busola");
 
-        System.out.println(name_of_students.get(3));
+        System.out.println(name_of_students.get(3)); 
         name_of_students.set(2, "Titilayo");
         name_of_students.remove(1);
         name_of_students.clear();
@@ -18,6 +19,36 @@ public class ArrayList{
         for(int i=0; i<name_of_students.size(); i++){
             System.out.println(name_of_students.get(i));
         }
+
+        for (String name : name_of_students) {
+            System.out.println(name);
+        }
+
+        LinkedList<Integer> age = new LinkedList<Integer>();
+        LinkedList<Boolean> age = new LinkedList<Integer>();
+        LinkedList<character> age = new LinkedList<character>();
+        LinkedList<Double> age = new LinkedList<Double>();
+        age.add(20);
+        age.add(25);
+        age.add(30);
+        age.add(35);
+        age.add(40);
+
+        Collections.sort(age);
+        for(int i : age) {
+            System.out.println(i);
+        }
+        Collections.sort(name_of_students);
+        for(String name : name_of_students) {
+            System.out.println(name);
+        }
+
+        age.addFirst(45)
+        age.addLast(45)
+        age.removeFirst(45)
+        age.removeLast(45)
+        age.getFirst(45)
+        age.getLast(45)
     }
 
 }
