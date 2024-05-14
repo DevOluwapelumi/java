2bcb37870692c90a7d9b37eda540aa0f34734ef6
MultiMarkTechPro Media <victoroluwadarasimi16@gmail.com>
2024-05-14 15:00:16 +0100

    Wrapper commit

---
+++ Wrapper.java
@@ -0,0 +1,33 @@
+// Java Wrapper classes //
+
+Premitive Data type             Wrapper Class
+byte                            byte
+short                           short
+int                             Integer
+long                            long
+float                           Float
+Double                          Double
+boolean                         Boolean
+char                            Character
+
+public class Wrapper{
+    public static void main(String[] args){
+        Interger age =56;
+        Double score = 87.5;
+        Character option = 'A';
+        String name = "Oluwapelumi";
+        Boolean status = True;
+
+       // System.out.printIn(age);
+       // System.out.printIn(score);
+       
+       // System.out.printIn(age.intValue());
+       // System.out.printIn(score.doubleValue());
+       // System.out.printIn(option.charValue());
+       // System.out.printIn(status.booleanValue());
+
+       // System.out.printIn(age.toString());
+       // System.out.printIn(name.length());
+
+    }
+}
