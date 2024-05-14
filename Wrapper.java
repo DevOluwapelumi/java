15acd9b92f1346accf50ac11415901bb127f1461
MultiMarkTechPro Media <victoroluwadarasimi16@gmail.com>
2024-05-14 15:06:51 +0100

    Exception commit

--- Wrapper.java
+++ Wrapper.java
@@ -1,26 +1,26 @@
 // Java Wrapper classes //
 
-Premitive Data type             Wrapper Class
-byte                            byte
-short                           short
-int                             Integer
-long                            long
-float                           Float
-Double                          Double
-boolean                         Boolean
-char                            Character
+// Premitive Data type             Wrapper Class
+// byte                            byte
+// short                           short
+// int                             Integer
+// long                            long
+// float                           Float
+// Double                          Double
+// boolean                         Boolean
+// char                            Character
 
 public class Wrapper{
     public static void main(String[] args){
-        Interger age =56;
+        // Interger age =56;
         Double score = 87.5;
         Character option = 'A';
         String name = "Oluwapelumi";
-        Boolean status = True;
+        // Boolean status = True;
 
        // System.out.printIn(age);
        // System.out.printIn(score);
-       
+
        // System.out.printIn(age.intValue());
        // System.out.printIn(score.doubleValue());
        // System.out.printIn(option.charValue());
