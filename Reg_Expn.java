ba91f701d4b383ab1988a7b1eb5af98dbf376dc3
MultiMarkTechPro Media <victoroluwadarasimi16@gmail.com>
2024-05-20 15:10:58 +0100

    Reg_Expn commit

---
+++ Reg_Expn.java
@@ -0,0 +1,27 @@
+        //  JavaRegularExpression   //  --- Abbreviate to "Reg_Expn"
+        //  This is java code it is called java regular expression A.K.A(REGEX) in javaScript   //
+
+
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Reg_Expn {
+    String comment = "This is a java class";
+
+    public static void main(String[] args) {
+        // Corrected to use java.util.regex.Pattern and java.util.regex.Matcher
+        Pattern pat = Pattern.compile("class", Pattern.CASE_INSENSITIVE);
+
+        Reg_Expn instance = new Reg_Expn(); // Create an instance to access the non-static
+                                                                      // variable
+        Matcher mat = pat.matcher(instance.comment);
+        boolean find = mat.find();
+
+        if (find) {
+            System.out.println("We have a match in the text");
+        } else {
+            System.out.println("No match found in the text");
+ }
+}
+}
